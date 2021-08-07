554c2c9907d0f2c55c20e4c7d91fbe17584ca63b
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-07 20:59:10 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,31 +1,25 @@
 let form = document.getElementById("form");
 form.addEventListener("submit",function(event){
     event.preventDefault();
-let username = document.getElementById("username");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let ConfirmPassword = document.getElementById("password2");
+    //Recuperation of the inputs Data
+        let username = document.getElementById("username");
+        let email = document.getElementById("email");
+        let password = document.getElementById("password");
+        let ConfirmPassword = document.getElementById("password2");
+    //Conditions on the Inputs
 if(username.value==="" || username.value.length<3 || username.value.length>15){
     username.style.border="2px solid red";
     const error1=document.getElementById("error1").textContent="You must use between 3 and 15 caracters";
-
-
 }else{
     username.style.border="2px solid fuchsia";
 }
 const expression =/^(([^<()[\]\\.,;:\s@\]+(\.[^<()[\]\\.,;:\s@\]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 if(expression.test(email.value)){
     email.style.border="2px solid fuchsia";
-   
-
 }else{
     email.style.border="2px solid red";
     const error2 = document.getElementById("error2").textContent="l'email n'est pas valide";    
 }
-
-
-
-
 if(password.value==="" || password.value.length<3 || password.value.length>15){
     password.style.border="2px solid red";
     const error3 = document.getElementById("error3").textContent="You must use between 3 and 15 caracters"
@@ -38,27 +32,4 @@ if(ConfirmPassword.value!=password.value ){
 }else{
     ConfirmPassword.style.border="2px solid fuchsia";
 }
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
