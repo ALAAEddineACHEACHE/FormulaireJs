e8e69642795aa19524e89c033effd6c2412337df
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-07 20:34:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let form = document.getElementById("form");
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let ConfirmPassword = document.getElementById("password2");
+if(username.value==="" || username.value.length<3 || username.value.length>15){
+    username.style.border="2px solid red";
+    const error1=document.getElementById("error1").textContent="You must use between 3 and 15 caracters";
+
+
+}else{
+    username.style.border="2px solid fuchsia";
+}
+const expression =/^(([^<()[\]\\.,;:\s@\]+(\.[^<()[\]\\.,;:\s@\]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+if(expression.test(email.value)){
+    email.style.border="2px solid fuchsia";
+   
+
+}else{
+    email.style.border="2px solid red";
+    const error2 = document.getElementById("error2").textContent="l'email n'est pas valide";    
+}
+
+
+
+
+if(password.value==="" || password.value.length<3 || password.value.length>15){
+    password.style.border="2px solid red";
+    const error3 = document.getElementById("error3").textContent="You must use between 3 and 15 caracters"
+}else{
+    password.style.border="2px solid fuchsia";
+}
+if(ConfirmPassword.value!=password.value ){
+    ConfirmPassword.style.border="2px solid red";
+    const error4 = document.getElementById("error4").textContent="Les deux mots de passes ne sont pas égaux";
+}else{
+    ConfirmPassword.style.border="2px solid fuchsia";
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
